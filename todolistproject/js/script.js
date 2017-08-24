d7010f78b5e71ffeb288f73c4b8e68e83b54d819
naveenpatti4 <30995561+naveenpatti4@users.noreply.github.com>
2017-08-24 11:48:03 -0400

    Add files via upload

---
+++ todolistproject/js/script.js
@@ -0,0 +1,26 @@
+//checking of todos by oning
+$('ul').on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//click on x to delete
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(event){
+if(event.which === 13){
+//getting value for todo from input
+    var todoText = $(this).val();
+    $(this).val("");
+//create a new li and appending it to ul
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+}
+});
+
+$(".fa-plus").click(function(){
+    $("input[type = 'text']").fadeToggle()
+});
